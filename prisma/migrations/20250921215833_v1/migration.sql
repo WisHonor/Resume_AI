53a89adc8d0994e0524cc24fbd578f638d66fdd1
Wissam Elmasry <elmasry.wissam@gmail.com>
2025-09-21 18:00:57 -0400

    fixed prisma issues

--- prisma/migrations/20250921215833_v1/migration.sql
+++ prisma/migrations/20250921215833_v1/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "public"."File" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
@@ -9,6 +20,12 @@ CREATE TABLE "public"."File" (
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "public"."User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "File_fileUrl_key" ON "public"."File"("fileUrl");
